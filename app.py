6b375bd322017c76a95085be51831fa4db5e7aae
JCBuarque <jcbuarque@hotmail.com>
2026-05-15 18:05:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title="Re Leve Ops AI", page_icon="🥗", layout="wide")
-
-st.title("Re Leve Ops AI")
-st.success("Aplicativo funcionando corretamente 🚀")
-st.write("Deploy realizado com sucesso no Streamlit.")
+st.title("Teste")
+st.write("Funcionando")
